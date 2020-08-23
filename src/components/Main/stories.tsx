ceba58bb08dabb56094a193444ae23580054982f
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-08-23 23:24:47 +0200

    Finish boilerplate

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Title default',
+    description: 'Description default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
-)
-
-export default Main
